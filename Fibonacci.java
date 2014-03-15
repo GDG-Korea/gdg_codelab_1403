18d4e000719fc26658493a3b10d00bccf68fe6b2
enghqii <enghqii@gmail.com>
2014-03-15 15:26:21 +0900

    Update Fibonacci.java
    parse exception handling, 'start' out of range handling

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,14 +3,29 @@ import java.math.BigInteger;
 public class Fibonacci{
 
      public static void main(String []args){
-         int start = Integer.parseInt(args[0]);
-         BigInteger fib1 = new BigInteger("0");
-         BigInteger fib2 = new BigInteger("1");
-         for(int i = 0; i <= (start -2); i++) {
-             BigInteger fib3 = fib1.add(fib2);
-             System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
-             fib1 = fib2;
-             fib2 = fib3;
-         }
+    	 
+    	 int start = 10;
+    	 
+    	 try{
+    	
+    		 if(args.length > 0){
+    			 start = Integer.parseInt(args[0]);
+    		 }
+    		 
+    	 }catch(NumberFormatException e){
+    		 e.printStackTrace();
+    	 }
+
+    	 if(start > 2){
+
+    		 BigInteger fib1 = new BigInteger("0");
+    		 BigInteger fib2 = new BigInteger("1");
+    		 for(int i = 0; i <= (start -2); i++) {
+    			 BigInteger fib3 = fib1.add(fib2);
+    			 System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
+    			 fib1 = fib2;
+    			 fib2 = fib3;
+    		 }
+    	 }
      }
 }
