8fe2f32b1234fc6e8dc6ed91f13d893aa4d69675
seonggwang.gwon <g.passcode@gmail.com>
2014-03-15 16:32:48 +0900

    결과 출력부분을 함수로 분리
    추후 테스트 케이스 등을 작성할 때 출력부분이 로직에 포함되면 불편할 것 같아
    분리하였습니다.
    
    Signed-off-by: seonggwang.gwon <g.passcode@gmail.com>

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,6 +5,10 @@ import java.math.BigInteger;
  * 피보나치 수열이 뭔가요? http://ko.wikipedia.org/wiki/%ED%94%BC%EB%B3%B4%EB%82%98%EC%B9%98_%EC%88%98
  */
 public class Fibonacci{
+     public static void Print(int index, BigInteger v1, BigInteger v2, BigInteger v3) {
+          System.out.println("F"+ index + " = " + v1 + ", F" + (index+1) + " = " + v2 + ", F" + (index+2) + " =  " + v3);
+     }
+
      public static void main(String []args){
          if (args.length == 0) {
              print_usage(Fibonacci.class.getName());
@@ -15,7 +19,7 @@ public class Fibonacci{
          BigInteger fib2 = new BigInteger("1");
          for(int i = 0; i <= (start -2); i++) {
              BigInteger fib3 = fib1.add(fib2);
-             System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
+             Print(i, fib1, fib2, fib3);
              fib1 = fib2;
              fib2 = fib3;
          }
