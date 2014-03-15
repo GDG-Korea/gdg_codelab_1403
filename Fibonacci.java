b9a2af7d9358dc5d63a6a5ef2f3d536a231387fc
Seoh Cha <devthewild@gmail.com>
2014-03-15 15:06:18 +0900

    인자 검증 추가
    0개 이하이거나 숫자가 아닐 때

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,14 +1,26 @@
-public class Fibonacci{
+public class Fibonacci {
 
-     public static void main(String []args){
-         int start = Integer.parseInt(args[0]);
-         int fib1 = 0;
-         int fib2 = 1;
-         for(int i = 0; i <= (start -2); i++) {
-             int fib3 = fib1 + fib2;
-             System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
-             fib1 = fib2;
-             fib2 = fib3;
-         }
-     }
+    public static void main(String[] args) {
+        if( args.length < 1 ) {
+            System.out.println("인자가 있어야 합니다.");
+            return;
+        }
+        
+        int start = -1;
+        try{
+            start = Integer.parseInt(args[0]);
+        }catch(NumberFormatException e){
+            System.out.println("인자는 숫자로 받아야 합니다.");
+            return;
+        }
+
+        int fib1 = 0;
+        int fib2 = 1;
+        for(int i = 0; i <= (start -2); i++) {
+            int fib3 = fib1 + fib2;
+            System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
+            fib1 = fib2;
+            fib2 = fib3;
+        }
+    }
 }
