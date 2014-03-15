4dc3dcc0aea4eaff4d27d7bffd1295e831bf75a2
Jongchan Kim <flashilver@gmail.com>
2014-03-15 14:51:31 +0900

    인자가 47이상일때 음수가 출력되는 문제 해결

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,9 +6,13 @@ public class Fibonacci{
          int fib2 = 1;
          for(int i = 0; i <= (start -2); i++) {
              int fib3 = fib1 + fib2;
-             System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
+             System.out.println("F"+ i + " = " + unsigned32(fib1) + ", F" + (i+1) + " = " + unsigned32(fib2) + ", F" + (i+2) + " =  " + unsigned32(fib3));
              fib1 = fib2;
              fib2 = fib3;
          }
+     }
+
+     public static long unsigned32(int n){
+        return n & 0xFFFFFFFFL;
      }
 }
