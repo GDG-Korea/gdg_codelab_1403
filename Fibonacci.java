ffa7ef468fb5de132304b3a38aff2e070b4dd4af
UEO <mjisnmd@gmail.com>
2014-03-15 16:02:27 +0900

    오오오오오오오ㅗㅇ오
    오오오오오오오오오오??

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,14 +1,28 @@
 public class Fibonacci{
 
-     public static void main(String []args){
-         int start = Integer.parseInt(args[0]);
-         int fib1 = 0;
-         int fib2 = 1;
-         for(int i = 0; i <= (start -2); i++) {
-             int fib3 = fib1 + fib2;
-             System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
-             fib1 = fib2;
-             fib2 = fib3;
-         }
-     }
+    public static void main(String []args){
+        try{
+            int start = Integer.parseInt("2");
+
+            double fib1 = 0;
+            double fib2 = 1;
+            if(start>=2 && start<47) {
+                for(int i = 0; i <= (start-2); i++) {
+                    double fib3 = fib1 + fib2;
+                    System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
+                    fib1 = fib2;
+                    fib2 = fib3;
+                }
+            } else if(start==1) {
+                System.out.println("F"+ start + " = " + fib2 );
+            } else if(start==0) {
+                System.out.println("F"+ start + " = " + fib1 );
+            } else{
+                throw new Exception();
+            }
+        }catch(Exception e){
+
+        }
+    }
+    
 }
