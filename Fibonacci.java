692df4760f2fd6d00713df0dc9a56fe10db224b2
leehk1227 <leehk1227@gmail.com>
2014-03-15 15:19:50 +0900

    fix integer range issue

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,13 +2,17 @@ public class Fibonacci{
 
      public static void main(String []args){
          int start = Integer.parseInt(args[0]);
-         int fib1 = 0;
-         int fib2 = 1;
+         long fib1 = 0;
+         long fib2 = 1;
          for(int i = 0; i <= (start -2); i++) {
-             int fib3 = fib1 + fib2;
-             System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
+             long fib3 = fib1 + fib2;
+             System.out.println("F"+ i + " = " + Fibonacci.unsigned32(fib1) + ", F" + (i+1) + " = " + Fibonacci.unsigned32(fib2) + ", F" + (i+2) + " =  " + unsigned32(fib3));
              fib1 = fib2;
              fib2 = fib3;
          }
      }
+
+	 public static long unsigned32(long number) {
+		 return number & 0xFFFFFFFFL;
+	 }
 }
