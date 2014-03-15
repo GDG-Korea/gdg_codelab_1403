85706eb28a91744236771517ccfe7220cd893917
eunj14 <choi.eunjeong@gmail.com>
2014-03-15 16:04:29 +0900

    메인함수와 테스트 함수 분리, 테스트 횟수 입력받기
    메인함수와 테스트 함수 분리, 테스트 횟수 입력받기

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,14 +1,49 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+
 public class Fibonacci{
 
-     public static void main(String []args){
-         int start = Integer.parseInt(args[0]);
-         int fib1 = 0;
-         int fib2 = 1;
-         for(int i = 0; i <= (start -2); i++) {
-             int fib3 = fib1 + fib2;
-             System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
-             fib1 = fib2;
-             fib2 = fib3;
-         }
+	public void test(int n) {
+	
+		Random r = new Random();
+		r.setSeed(10);
+        
+        BigInteger fib1 = new BigInteger("0");
+        BigInteger fib2 = new BigInteger("1");
+        BigInteger fib3 = new BigInteger("0");
+        
+        int start = 0;
+        
+		for (int e = 0; e < n; e++) {
+			start = r.nextInt(100);
+			System.out.println("\n\n1 => " + start);
+			fib1 = BigInteger.ZERO;
+			fib2 = BigInteger.ONE;
+			fib3 = BigInteger.ZERO;
+			
+	        for(int i = 0; i <= (start -2); i++) {
+	            fib3 = fib1.add(fib2);
+	            System.out.println("F"+ i + " = " + fib1 + ", F" + (i+1) + " = " + fib2 + ", F" + (i+2) + " =  " + fib3);
+	            fib1 = fib2;
+	            fib2 = fib3;
+	        }
+		}
+   	
+	}
+	
+     public static void main(String []args)
+     throws IOException {
+    	 
+    	 System.out.print("# : ");
+    	 BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
+    	 String no = bi.readLine();
+    	 int n = Integer.parseInt(no);
+    	 
+    	 Fibonacci f = new Fibonacci();
+    	 f.test(n);
+    	 
+         bi.close();
      }
 }
