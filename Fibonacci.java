7fba61c3c0a0683ee5a9b505cd48f619cc9e5b67
corikachu <konomama0505@gmail.com>
2014-03-15 16:33:16 +0900

    주석을 한글에서 영어로 바꾸어 보았습니다

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,8 +1,8 @@
 import java.math.BigInteger;
 
 /**
- * 피보나치 수열을 구하는 클래스입니다.
- * 피보나치 수열이 뭔가요? http://ko.wikipedia.org/wiki/%ED%94%BC%EB%B3%B4%EB%82%98%EC%B9%98_%EC%88%98
+ * This class is to calculate the Fibonacci Sequence
+ * What is the Fibonacci Sequence? http://ko.wikipedia.org/wiki/%ED%94%BC%EB%B3%B4%EB%82%98%EC%B9%98_%EC%88%98
  */
 public class Fibonacci{
      public static void main(String []args){
